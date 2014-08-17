d2625495aa14445fa80fcdd59df063a89592d4be
Stephen Crawley <crawley.stephen@gmail.com>
2014-08-18 01:04:40 +1000

    Install the apache2.conf

--- recipes/drupal_install_package.rb
+++ recipes/drupal_install_package.rb
@@ -30,15 +30,20 @@
 version = node['drupal']['version'] || '7'
 install_dir = node['apache']['docroot_dir'] 
 
-case version 
-when 'latest', '7' then
-  package 'drupal7' do
-    action :install
-  end
-when '6' then
-  package 'drupal6' do
-    action :install
-  end
+case version
+when 'latest'
+  version = '7' 
+when '6', '7' then
+  # OK
 else
   raise "Don't know how to install Drupal version '#{drupal}' from packages"
+end
+
+package 'drupal#{version}' do
+  action :install
+end
+
+bash 'install drupal.conf' do
+  code "cp /etc/drupal/#{version}/apache2.conf /etc/apache2/mods-enabled/drupal.conf"
+  notifies :restart, "service[apache2]", :delayed
 end
