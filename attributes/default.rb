055feb784ed5abe5dc7cacc7ccfdaf2f8895dfdf
Stephen Crawley <crawley.stephen@gmail.com>
2014-08-19 00:55:46 +1000

    Added a database for testing ...

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,12 @@
 node.default['drupal']['version'] = 'latest'
 node.default['drupal']['install_method'] = 'package'
+node.default['drupal']['databases'] = {
+  'default/default' => {
+    'driver' => 'mysql',
+    'database' => 'test',
+    'username' => 'foo',
+    'password' => 'secret',
+    'host' => 'localhost',
+    'prefix' => ''
+  }
+}
