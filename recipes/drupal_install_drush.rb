6b10475d98baf4eec53b0b9235da6573dab21ff6
Stephen Crawley <crawley.stephen@gmail.com>
2014-09-13 01:28:20 +1000

    Try using drush to download drupal

--- recipes/drupal_install_drush.rb
+++ recipes/drupal_install_drush.rb
@@ -32,23 +32,23 @@ version = drupal['version'] || '7'
 install_dir = node['apache']['docroot_dir'] 
 
 case version
-when 'latest'
-  version = '7' 
-when '6', '7' then
-  # OK
-else
-  raise "Don't know how to install Drupal version '#{drupal}' from packages"
+when 'latest', '7'
+  version = 'drupal-7' 
+when '6' then
+  version = 'drupal-6'
+when 'dev' then
+  version = 'drupal-7.x'
 end
 
 drupal_installation = "/usr/share/drupal#{version}"
 $drupal_sites = "/usr/share/drupal#{version}/sites"
 
-package "drupal#{version}" do
-  action :install
-end
-
 include_recipe "drush::pear"
 
+drush_cmd "dl" do
+  arguments version
+  drupal_root drupal_installation  
+end
 
 db = drupal['databases']['default/default']
 db_url = "mysql://#{db['username']}:#{db['password']}@" +
